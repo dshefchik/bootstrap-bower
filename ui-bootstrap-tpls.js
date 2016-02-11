c4c4c140d0420fef474ce2b8a628f402fc38c602
David Shefchik <dshefchik@gmail.com>
2016-02-11 13:02:58 -0800

    Update ui-bootstrap-tpls.js
    Add non-minified hacky, bullshit ie modal fix

--- ui-bootstrap-tpls.js
+++ ui-bootstrap-tpls.js
@@ -2644,7 +2644,7 @@ angular.module('ui.bootstrap.modal', [])
         //move focus to specified element if available, or else to body
         if (elementToReceiveFocus && elementToReceiveFocus.focus) {
           elementToReceiveFocus.focus();
-        } else {
+        } else if (body && body.focus){
           body.focus();
         }
       }
